9a6dd5a4fab7c52abf2c57bf3f9be371376744e7
hp <hp@LAPTOP-GAA1RH4E>
2022-06-17 11:17:20 +0100

    Add project files.

---
+++ RadiusOfACircle/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadiusOfACircle
+{
+     class Circle
+    {
+        const double pi = 3.14159;
+        double radius;
+        
+
+        public void AcceptRadius()
+        {
+            Console.Write("Enter the radius of the circle: ");
+             radius = double.Parse(Console.ReadLine());
+            
+        }
+        public double GetArea()
+        {
+            return pi * radius * radius;
+        }
+        public void ShowResult()
+        {
+            Console.WriteLine("The radius of the circle is : {0}", radius);
+            Console.WriteLine($"Area: {GetArea():2F}");
+        }
+    }
+}
+ 
---
+++ RadiusOfACircle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RadiusOfACircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle oneCircle = new Circle();
+            oneCircle.AcceptRadius();
+            oneCircle.GetArea();
+            oneCircle.ShowResult();
+            Console.ReadKey();
+        }
+    }
+}
